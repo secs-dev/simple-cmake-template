00d0901a5a73ff575f59928beb7c39ea77cdccb7
e1turin <ivturnik2003@gmail.com>
2024-12-07 00:28:08 +0300

    simple template

---
+++ app/app.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world from app.c\n");
+    return 0;
+}
---
+++ app/app.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world from app.cpp" << std::endl;
+    return 0;
+}
